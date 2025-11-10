aca202b1b601d4bbe51869d84c4e620a1c47cee8
Josue Palma <josuepalma23@users.noreply.github.com>
2025-11-09 23:17:11 -0500

    Parte de la implementacion de java completada

---
+++ algoritmosOrdenamiento.java
@@ -0,0 +1,15 @@
+import java.util.Arrays;
+
+/*
+ * elegimos los algoritmos quicksort y mergesort
+ * por su velocidad y flexibilidad
+ * Quick Sort --> O(N log N)
+ * Merge Sort --> O(N log N)
+ */
+
+public class algoritmosOrdenamiento {
+    /*
+     * falta implementar toda esa vaina
+     * 
+     */
+}
