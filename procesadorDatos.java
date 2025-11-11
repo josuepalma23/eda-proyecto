e75bb8adf410e6b865c79cdac2d201cf74a80730
Josue Palma <josuepalma23@users.noreply.github.com>
2025-11-11 01:20:04 -0500

    solo falta imprimir el archivo y manejar la excepcion

--- procesadorDatos.java
+++ procesadorDatos.java
@@ -4,7 +4,6 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Hashtable;
 import java.util.List;
-import java.util.Map;
 
 
 public class procesadorDatos{
@@ -27,10 +26,48 @@ public class procesadorDatos{
                     String ciudad = columnas[1];
                     double temp = Double.parseDouble(columnas[2]);
                     String anioStr = columnas[3].substring(0, 4);
+                    String clave = ciudad + "-" + anioStr;
 
-                    
+                    /*
+                     * computeIfAbsent comprueba rapidamente si existe una una entrada del
+                     * agregador para una clave, si existe, la obtiene, si no, crea una nueva,
+                     *  la ubica en la tabla y luego la obtiene
+                     */
+                    agregadorTemperatura agregadorNuevo = promediosCiudadYAnio.computeIfAbsent(
+                        clave, k -> new agregadorTemperatura());
+                    agregadorNuevo.agregar(temp); //incrementar el conteo de la suma total del agregador
+
+                } catch (Exception e){
+
                 }
             }
         }
+    }
+
+
+    public List<ciudadesYPromedio> getResultadosFormula(){
+        List<ciudadesYPromedio> resultados = new ArrayList<>();
+
+        //obtenemos las claves
+        for(String clave : promediosCiudadYAnio.keySet()){
+            //obtener los valores de cada clave
+            agregadorTemperatura agregador = promediosCiudadYAnio.get(clave);
+
+            //procesar el resultado
+
+            /*
+             * Estas lineas son importantes porq nos ayuda a reconstruir los datos desde la clave
+             * "Portoviejo-2018" --> ["Portoviejo", "2018"]
+             * asi se recupera de nuevo la informacion.
+             */
+            String[] claveSplit = clave.split("-");
+            String ciudad = claveSplit[0];
+            int anio = Integer.parseInt(claveSplit[1]);
+            double promedio = agregador.getPromedio();
+
+            resultados.add(new ciudadesYPromedio(ciudad, anio, promedio));
+
+        }
+        return resultados;
     }
 }
