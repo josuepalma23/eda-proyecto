e9c05ee8fc51cb12f3f59d07d0b2eecab5110fbb
Josue Palma <josuepalma23@users.noreply.github.com>
2025-11-22 01:12:16 -0500

    errores en la linea 123, 137,138 maniana arreglo

--- procesamientoTemperaturas.java
+++ procesamientoTemperaturas.java
@@ -1,4 +1,5 @@
 import java.io.*;
+import java.lang.reflect.Array;
 import java.util.*;
 import java.util.regex.Pattern;
 
@@ -83,6 +84,79 @@ public class procesamientoTemperaturas{
     }
 
     public static void main(String[] args) {
-        
+        //inicio del programa calculado al inicio
+        long tiempoInicio = System.nanoTime();
+
+        String archivoEntrada = "nuevo.csv";
+        String archivoSalida = "procesamiento_java.csv";
+
+        System.out.println("Procesamiento Simple");
+        long inicioLectura = System.nanoTime();
+
+        ArrayList<Registro> bufferLectura = new ArrayList<>();
+        //para los datos con parentesis y guiones y eso
+        Pattern limpiador = Pattern.compile("[\"\\(\\)-]");
+
+        try (BufferedReader br = new BufferedReader(new FileReader(archivoEntrada))){
+            String linea = br.readLine(); //leer la primera linea osea el encabezaod
+
+            while ((linea = br.readLine()) != null) {
+                if (linea.trim().isEmpty()) {
+                    continue;
+                }
+
+                String[] partes = linea.split(",");
+
+                //si faltan columnas se considera invalido
+                if (partes.length < 4) {
+                    bufferLectura.add(new Registro("", "", 0.0, false));
+                    continue;
+                }
+
+                try {
+                    String brutoTemp = partes[2]
+                                        .replace("\"", "")
+                                        .replace("(", "")
+                                        .replace(")", "")
+                                        .replace("-", "");
+                    
+                    String temperatura = Double.parseDouble(brutoTemp);
+
+                    String brutoFecha = partes[3]
+                                        .replace("\"", "")
+                                        .replace("(", "")
+                                        .replace(")", "");
+
+                    String[] parteFecha = brutoFecha.split("/");
+                    String anio = (parteFecha.length >= 3) ? parteFecha[2] : "0000";
+
+                    //limpiar ciudades de las comillas
+                    String ciudad = partes[1].replace("\"", "");
+
+                    //filtro de las temperaturas
+                    if (temperatura > -90) {
+                        bufferLectura.add(new Registro(ciudad, anio, temperatura, false));
+                    } else {
+                        bufferLectura.add(new Registro("", "", 0.0, false));
+                    }
+
+                } catch (Exception e ){
+                    bufferLectura.add(new Registro("", "", 0.0, false));
+
+                }
+            }
+        } catch (IOException e ){
+            System.out.println("Error al procesar " + e.getMessage());
+            return;
+        }
+
+        Registro[] datosCrudos = bufferLectura.toArray(new Registro[0]);
+        bufferLectura = null;
+
+        long finLectura = System.nanoTime();
+        System.out.println("Tiempo de lectura de datos: " + (finLectura - inicioLectura) / 1_000_000_000.0);
+        System.out.println("Cantidad de datos: " + datosCrudos.length);
+
+
     }
 }
